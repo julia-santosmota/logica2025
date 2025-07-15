b1477c45467c6d7c7e21948f2834abca5f739a01
Julia Mota <julia.mota@unifesp.br>
2025-07-15 17:58:59 -0300

    converte numero decimal para decimal

---
+++ coverteBinario.c
@@ -0,0 +1,30 @@
+// converte numero decimal para binario
+#include <stdio.h>
+
+void converte_para_binario(int n) {
+    if (n < 2) {
+        printf("%d", n);
+        return; 
+    } 
+    else {
+        converte_para_binario(n / 2);
+        printf("%d", n % 2);
+    }
+}
+
+int main() {
+    int numero_decimal;
+
+    printf("Digite um numero decimal inteiro para converter para binario: ");
+    scanf("%d", &numero_decimal);
+
+    if (numero_decimal < 0) {
+        printf("Este algoritmo funciona somente para numeros positivos.\n");
+    } else {
+        printf("A representacao binaria de %d e: ", numero_decimal);
+        converte_para_binario(numero_decimal);
+        printf("\n"); 
+    }
+
+    return 0;
+}
